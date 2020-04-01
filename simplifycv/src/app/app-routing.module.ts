c4b85fbcc57c53861f3722affa2af3e751681c6f
@Shruthi123 <shruthi21g@gmail.com>
2020-04-01 13:28:23 +0530

    landing page and dummy directive

--- simplifycv/src/app/app-routing.module.ts
+++ simplifycv/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component'
 import {ProfileComponent} from './profile/profile.component'
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
-  { path: '',redirectTo:'home',pathMatch:'full'},
+  { path: '',redirectTo:'start',pathMatch:'full'},
+  { path: 'start', component: LandingPageComponent},
   { path: 'home', component: HomeComponent},
   {path:'profile',component:ProfileComponent}
 ];
