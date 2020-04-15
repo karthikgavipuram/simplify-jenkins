4e16b509e66d7962f360030e7c8da28a6eec502a
Shane_ap <shanepereira459@gmail.com>
2020-04-15 17:21:22 +0530

    upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fileSystem = require('fs');
+const ls = spawn("python3",["parser.py",doc],{shell: true});
 
 router.post('/uploadResume', async function (req, res) {
     if (!req.files) {
@@ -12,12 +13,27 @@ router.post('/uploadResume', async function (req, res) {
         fileSystem.mkdirSync(folderPath);
     }
     let uid = 100000 + Math.floor(Math.random() * 900000)
-    let filename = `CV - user${id} - ${uid}`
+    let filename = `CV-user${id}-${uid}`
     filename += req.files["file"].name.substr(req.files["file"].name.indexOf("."), req.files["file"].name.length)
     let filepath = folderPath + "/" + filename;
     let usfile = req.files["file"];
     await usfile.mv(filepath);
-    res.status(200).json({ success: true, downloadUrl: filepath });
+    const ls = spawn("python3",["parser.py",doc],{shell: true});
+    ls.stdout.on("data", data => {
+        res.status(200).json({ success: true, downloadUrl: filepath, org_name:req.files["file"].name, parsedCv: data});
+    });
+
+    ls.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('error', (error) => {
+        console.log(`error: ${error.message}`);
+    });
+
+    ls.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+    });
 });
 
 module.exports = router
