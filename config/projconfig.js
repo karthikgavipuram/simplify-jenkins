eec1ebf898621c0774f4d3701e92248f3c937d68
mark <markchakravarthi97@gmail.com>
2020-04-21 11:20:40 +0530

    db creds

--- config/projconfig.js
+++ config/projconfig.js
@@ -3,8 +3,19 @@ const config = {
         
     },
     "default":{
-        redirectUrl : ["https://cv.simplifyqa.com"],
-        mongoCvUrl: "mongodb://172.105.58.154:27017/cv"
+        redirectUrl : ["http://localhost:4444"],
+        mongoCvUrl: "mongodb://db_cv:cvauth$er>r@172.105.58.154:27017/cv",
+        jwt_secret: {
+            "secretkey": "thisissecret"
+        },
+        email_set: {
+            "smtp_host": "send.one.com",
+            "smtp_port": 465,
+            "secure": true,
+            "mail_from": "noreply@simplify3x.com",
+            "simplify3x_username": "noreply@simplify3x.com",
+            "simplify3x_password": "simplify123"
+        }
     }
 }
 
