64079e754d6ce7eab337e8a49dde1c70b7885d71
@Shruthi123 <shruthi21g@gmail.com>
2020-04-25 17:37:53 +0530

    pb_certificate

--- simplifycv/src/app/profile-builder/profile-builder.component.ts
+++ simplifycv/src/app/profile-builder/profile-builder.component.ts
@@ -8,206 +8,232 @@ declare var $: any;
   styleUrls: ['./profile-builder.component.scss']
 })
 export class ProfileBuilderComponent implements OnInit {
-  cvDetails:any;
-  message:string="Successfully saved entered data";
-  header:string='Personal Information';
-  subHeader:string="Basic Info";
-  certificationHeader:string="Certifications";
-  subProjHeader:boolean=false;
-  rows:any=[];
-  showPersonal:boolean=true;
-  showSummary:boolean=false;
-  workExp:any={"company":"","designation":"","engagementType":"","from":{"year":"","month":""},"to":{"year":"","month":""},"location":""};
-  education:any={"institute":"","course":"","from":"","to":"","grade":{"type":"","value":""},"level":"","location":""};
-  project:any={"name":"","role":"","skills_used":[],"domain":"","type":"","summary":"","responsibility":"" ,"ongoing":false,"from":{"year":"","month":""},"to":{"year":"","month":""},"location":""};
-  certification:any={"title":"","organization":"","issue_date":"","expires":true,"expiry_date":"","credential_id":"","url":""};
-  sections:any=['Personal Information','Education','Work Experience','Skills','Domain','Projects','Certification and Achievements'];
-  subSection=['Basic Info', 'Job Specific Info'];
-  noticePeriod=[{"key":"Actively Looking","value":"active"},{"key":"Passively Looking","value":"passive"},{"key":"Serving Notice Period","value":"serving"},{"key":"Immediately Available","value":"immediate"}];
-  expertise_level=[{"key":"Aware","value":"aware"},{"key":"Expert","value":"expert"},{"key":"Applied","value":"applied"}];
-  currency=[{"key":"INR","value":"INR"},{"key":"USD","value":"USD"},{"key":"CAD","value":"CAD"},{"key":"AUD","value":"AUD"},{"key":"GBP","value":"GBP"}];
-  countries=[{"key":"USA","value":"USA"},{"key":"Canada","value":"Canada"},{"key":"UK","value":"UK"},{"key":"Australia","value":"Autralia"},{"key":"Dubai","value":"Dubai"}];
-  qualification=[{"key":"High School","value":"High School"},{"key":"Intermediate","value":"Intermediate"},{"key":"UG","value":"UG"},{"key":"PG","value":"PG"},{"key":"PhD","value":"PhD"}];
-  grade=[{"key":"GPA","value":"GPA"},{"key":"Percentage","value":"Percentage"},{"key":"CGPA","value":"CGPA"}];
-  engagementType=[{"key":"Full Time","value":"Full Time"},{"key":"Part Time","value":"Part Time"},{"key":"Intern","value":"Intern"}];
+  cvDetails: any;
+  message: string = "Successfully saved entered data";
+  header: string = 'Personal Information';
+  subHeader: string = "Basic Info";
+  certificationHeader: string = "Certifications";
+  subProjHeader: boolean = false;
+  rows: any = [];
+  showPersonal: boolean = true;
+  showSummary: boolean = false;
+  workExp: any = { "company": "", "designation": "", "engagementType": "", "from": { "year": "", "month": "" }, "to": { "year": "", "month": "" }, "location": "" };
+  education: any = { "institute": "", "course": "", "from": "", "to": "", "grade": { "type": "", "value": "" }, "level": "", "location": "" };
+  project: any = { "name": "", "role": "", "skills_used": [], "domain": "", "type": "", "summary": "", "responsibility": "", "ongoing": false, "from": { "year": "", "month": "" }, "to": { "year": "", "month": "" }, "location": "" };
+  certification: any = { "title": "", "organization": "", "issue_date": "", "expires": true, "expiry_date": "", "credential_id": "", "url": "" };
+  sections: any = ['Personal Information', 'Education', 'Work Experience', 'Skills', 'Domain', 'Projects', 'Certification and Achievements'];
+  subSection = ['Basic Info', 'Job Specific Info'];
+  noticePeriod = [{ "key": "Actively Looking", "value": "active" }, { "key": "Passively Looking", "value": "passive" }, { "key": "Serving Notice Period", "value": "serving" }, { "key": "Immediately Available", "value": "immediate" }];
+  expertise_level = [{ "key": "Aware", "value": "aware" }, { "key": "Expert", "value": "expert" }, { "key": "Applied", "value": "applied" }];
+  currency = [{ "key": "INR", "value": "INR" }, { "key": "USD", "value": "USD" }, { "key": "CAD", "value": "CAD" }, { "key": "AUD", "value": "AUD" }, { "key": "GBP", "value": "GBP" }];
+  countries = [{ "key": "USA", "value": "USA" }, { "key": "Canada", "value": "Canada" }, { "key": "UK", "value": "UK" }, { "key": "Australia", "value": "Autralia" }, { "key": "Dubai", "value": "Dubai" }];
+  qualification = [{ "key": "High School", "value": "High School" }, { "key": "Intermediate", "value": "Intermediate" }, { "key": "UG", "value": "UG" }, { "key": "PG", "value": "PG" }, { "key": "PhD", "value": "PhD" }];
+  grade = [{ "key": "GPA", "value": "GPA" }, { "key": "Percentage", "value": "Percentage" }, { "key": "CGPA", "value": "CGPA" }];
+  engagementType = [{ "key": "Full Time", "value": "Full Time" }, { "key": "Part Time", "value": "Part Time" }, { "key": "Intern", "value": "Intern" }];
   currentSection;
-  duration:any={};
-  years:any=[];
-  userObject:any={};
-  constructor(private _cs:AppService) { 
-    let today=new Date();
-    let year=today.getFullYear();
-    for(let i=1980;i<=year;i++){
-     this.years.push(i);
+  duration: any = {};
+  years: any = [];
+  userObject: any = {};
+  constructor(private _cs: AppService) {
+    let today = new Date();
+    let year = today.getFullYear();
+    for (let i = 1980; i <= year; i++) {
+      this.years.push(i);
     }
   }
-  nextSection(){
-    let section=this.sections.indexOf(this.header);
-    let section1=this.subSection.indexOf(this.subHeader);
-    document.getElementById(this.subHeader).style.color='unset';
-    if(section==0 && section1==0){
-        this.subHeader=this.subSection[1];
-        document.getElementById(this.subHeader).style.color='blue';
+  nextSection() {
+    let section = this.sections.indexOf(this.header);
+    let section1 = this.subSection.indexOf(this.subHeader);
+    document.getElementById(this.subHeader).style.color = 'unset';
+    if (section == 0 && section1 == 0) {
+      this.subHeader = this.subSection[1];
+      document.getElementById(this.subHeader).style.color = 'blue';
+      return;
+    }
+    document.getElementById(this.header).style.color = 'unset';
+    if (section == 0 || section1 > 0) {
+      let index = ++section;
+      if (index > 6)
         return;
-    }
-    document.getElementById(this.header).style.color='unset';
-    if(section==0 || section1>0){
-    let index=++section;
-  if(index>6)
-    return;
-    this.currentSection=this.sections[index];
-    this.header=this.currentSection;
-    document.getElementById(this.header).style.color='blue';
+      this.currentSection = this.sections[index];
+      this.header = this.currentSection;
+      document.getElementById(this.header).style.color = 'blue';
     }
   }
-  
+
   // ngOnInit() {
 
   // }
   ngOnInit() {
-    if(!this._cs.userDetails || !this._cs.userDetails.id){
+    if (!this._cs.userDetails || !this._cs.userDetails.id) {
       this._cs.getcookie().subscribe(
-        (res:any)=>{
-          if(res.token) localStorage.setItem('token',res.token)
+        (res: any) => {
+          if (res.token) localStorage.setItem('token', res.token)
           this._cs.setUserDetails();
-          this._cs.getData({"collection":"user",query:{"userId":this._cs.userDetails.id}}).subscribe((res:any)=>{
-            this.cvDetails=res.body.data[0].cvdetails.builderDetails;
-            this.userObject=res.body.data[0];
+          this._cs.getData({ "collection": "user", query: { "userId": this._cs.userDetails.id } }).subscribe((res: any) => {
+            this.cvDetails = res.body.data[0].cvdetails.builderDetails;
+            this.userObject = res.body.data[0];
             console.log(this.cvDetails);
           });
         },
-        (err)=>{
+        (err) => {
           console.log('error in getcookie()')
         }
       )
     }
-    else{
-      this._cs.getData({"collection":"user",query:{"userId":this._cs.userDetails.id}}).subscribe((res:any)=>{
-        this.cvDetails=res.body.data[0].cvdetails.builderDetails;
+    else {
+      this._cs.getData({ "collection": "user", query: { "userId": this._cs.userDetails.id } }).subscribe((res: any) => {
+        this.cvDetails = res.body.data[0].cvdetails.builderDetails;
         console.log(this.cvDetails);
-        this.userObject=res.body.data[0];
+        this.userObject = res.body.data[0];
       });
     }
     // setTimeout(() => {
     //   document.getElementById(this.subHeader).style.color='blue';
     // }, 200);
-    this.duration.months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    this.duration.years=this.years;
+    this.duration.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.duration.years = this.years;
   }
-  addEducation(data){
-    
-    this.cvDetails.education.push(this.education);
-    this.education={"institute":"","course":"","from":"","to":"","grade":{"type":"","value":""},"level":"","location":""};
-    
+  addEducation(data) {
+    this.cvDetails.education.push(this.clone(this.education));
+    this.hideModal('educationModal')
   }
-  editEdu(row,i){
-    this.education=row;
+  editEdu(row, i) {
+    this.education = row;
     this.showModal('educationModal');
   }
-  deleteEdu(i){
-    this.cvDetails.education.splice(i,1);
+  deleteEdu(i) {
+    this.cvDetails.education.splice(i, 1);
   }
-  addWork(workData){
-    
-    this.cvDetails.experience.push(this.workExp);
-    this.workExp={};
+  addWork(workData) {
+    if(this.workExp.currently_working){
+      this.workExp.to={"year":"","month":""};
+    }
+
+    this.cvDetails.experience.push(this.clone(this.workExp));
+    this.hideModal('workModal')
   }
-  editWork(row,i){
-    this.workExp=row;
+  clone(json){
+    return JSON.parse(JSON.stringify(json));
+  }
+  editWork(row, i) {
+    this.workExp = row;
     this.showModal('workModal');
   }
-  deleteWork(i){
-    this.cvDetails.experience.splice(i,1);
+  deleteWork(i) {
+    this.cvDetails.experience.splice(i, 1);
   }
-  addProject(projectData){
-    this.project.skills_used=this.project.skills.split(',');
+  addProject(projectData) {
+    if(this.project.ongoing){
+      this.project.to={"year":"","month":""};
+    }
+    if(this.project.skills)
+      this.project.skills_used = this.project.skills.split(',');
     delete this.project.skills;
-    this.cvDetails.projects.push(this.project);
-    this.project={};
+
+    this.cvDetails.projects.push(this.clone(this.project));
+    this.hideModal('projectModal')
   }
-  editProject(row,i){
-    this.project=row;
+  editProject(row, i) {
+    this.project = row;
     this.showModal('projectModal');
   }
-  deleteProject(i){
-    this.cvDetails.projects.splice(i,1);
+  deleteProject(i) {
+    this.cvDetails.projects.splice(i, 1);
   }
-  addCertificate(workData){
-   
-    this.cvDetails.certifications.push(this.certification);
-    
+  addCertificate(workData) {
+    if(this.certification.expires){
+      this.certification.expiry_date="";
+    }
+    this.cvDetails.certifications.push(this.clone(this.certification));
+    this.hideModal('certificationModal')
+
   }
-  editCertificate(row,i){
-    this.certification=row;
+  editCertificate(row, i) {
+    this.certification = row;
     this.showModal('certificationModal');
   }
-  deleteCertificate(i){
-    this.cvDetails.certifications.splice(i,1);
+  deleteCertificate(i) {
+    this.cvDetails.certifications.splice(i, 1);
   }
-  showModal(id){
-    $(".modal").modal('hide');
-    $("#" + id).modal();
+  showModal(id) {
+    // $(".modal").modal('hide');
+    $("#" + id).modal('show');
   }
-  showFields(header){
+  showFields(header) {
     // document.getElementById(this.header).style.color='unset';
     // document.getElementById(this.subHeader).style.color='unset';
-    if(this.subProjHeader){
-      this.subProjHeader=false;
+    if (this.subProjHeader) {
+      this.subProjHeader = false;
     }
-    this.header=header;
+    this.header = header;
     // let x =document.getElementById(this.header);
     // x.style.color='blue';
   }
-  showRow(card){
-    if(card=='skills'){
-      this.cvDetails.skills.push({"name":"","expertise_level":"","years":""});
+  showRow(card) {
+    if (card == 'skills') {
+      this.cvDetails.skills.push({ "name": "", "expertise_level": "", "years": "" });
     }
-    if(card=='domain'){
-      this.cvDetails.domain.push({"name":"","expertise_level":"","years":""});
-    }
-  }
-  deleteRow(card,i){
-    if(card=='skills'){
-      this.cvDetails.skills.splice(i,1);
-    }
-    if(card=='domain'){
-      this.cvDetails.domain.splice(i,1);
+    if (card == 'domain') {
+      this.cvDetails.domain.push({ "name": "", "expertise_level": "", "years": "" });
     }
   }
-  cancel(header){
-    if(header=='education'){
-        this.education={};
+  deleteRow(card, i) {
+    if (card == 'skills') {
+      this.cvDetails.skills.splice(i, 1);
+    }
+    if (card == 'domain') {
+      this.cvDetails.domain.splice(i, 1);
+    }
   }
-  if(header=='experience'){
-    this.workExp={};
+  clearJson(json) {
+    for (let key in json) {
+      if (typeof json[key] == 'string') json[key] = ""
+      else if (typeof json[key] == 'boolean') json[key] = false
+      else if (typeof json[key] == 'number') json[key] = null
+      else if (typeof json[key] == 'object'){
+        this.clearJson(json[key])
+      }
+    }
   }
-  if(header=='project'){
-    this.project={};
+
+  hideModal(id) {
+    $('#' + id).modal('hide');
+    if (id == 'educationModal') {
+      this.clearJson(this.education)
+    } else if (id == 'workModal') {
+      this.clearJson(this.workExp)
+    } else if (id == 'projectModal') {
+      this.clearJson(this.project)
+    } else if (id == 'certificationModal') {
+      this.clearJson(this.certification)
+    }
   }
-  if(header=='certification'){
-    this.certification={};
-}
-}
-  subHeaderChange(header){
-    this.header='Personal Information';
+
+
+  subHeaderChange(header) {
+    this.header = 'Personal Information';
     // document.getElementById(this.subHeader).style.color='unset';
-    this.subHeader=header;
+    this.subHeader = header;
   }
-  projectExpand(id){
-    this.showSummary=!this.showSummary;
+  projectExpand(id) {
+    this.showSummary = !this.showSummary;
     let x = document.getElementById(id);
-    if(this.showSummary){
-    x.style.height="85%";
+    if (this.showSummary) {
+      x.style.height = "85%";
+    }
+    else {
+      x.style.height = "30%";
+    }
   }
-  else{
-    x.style.height="30%";
-  }
-  }
-  save(){
-    this.userObject.cvdetails.builderDetails=this.cvDetails;
-    this._cs.updateObject({"collection":"user",query:{"userId":this._cs.userDetails.id},"updateFields":{"cvdetails":this.userObject.cvdetails}}).subscribe(()=>{
+  save() {
+    if(!this.cvDetails.personal_info.job_info.relocation){
+      this.cvDetails.personal_info.job_info.preferred_location="";
+    }
+    this.userObject.cvdetails.builderDetails = this.cvDetails;
+    this._cs.updateObject({ "collection": "user", query: { "userId": this._cs.userDetails.id }, "updateFields": { "cvdetails": this.userObject.cvdetails } }).subscribe(() => {
 
     });
   }
-  
+
 }
